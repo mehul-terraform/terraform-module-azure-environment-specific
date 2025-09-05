a9778eaca557f8b3376d7ace8a2e156583f53403
Your Name <you@example.com>
2025-09-05 15:27:30 +0530

    addmodules

--- Environments/Stage/backend.tf
+++ Environments/Stage/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "myexample-tf-rg01"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    resource_group_name  = "myexample-tf-rg01"       # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "myexampletfstorage01"    # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "myexample-stage-tfstate" # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "terraform.tfstate"     # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "terraform.tfstate"       # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
