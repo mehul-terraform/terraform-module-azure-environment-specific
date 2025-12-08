f242ff2148fbe5963f6ee073f99105a96023c926
Your Name <you@example.com>
2025-12-08 21:08:04 +0530

    add modules working

--- Environments/Dev/main.tf
+++ Environments/Dev/main.tf
@@ -13,8 +13,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = "339e9158-9454-4c79-a362-c37d1f2469a2"  
-  #subscription_id = "c09e0f60-cb15-4c23-8500-eeae1ec9dd6b"
+  #subscription_id = "339e9158-9454-4c79-a362-c37d1f2469a2"  
+  subscription_id = "c09e0f60-cb15-4c23-8500-eeae1ec9dd6b"
   #skip_provider_registration = true
   #  subscription_id = "c09e0f60-cb15-4c23-8500-eeae1ec9dd6b" # "az account show --query id -o tsv"
 }
