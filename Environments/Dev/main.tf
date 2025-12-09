f9f013789ac5ad3922b4f40e1ee47a78eefc852e
Your Name <you@example.com>
2025-12-09 14:08:48 +0530

    Remove hardcoded secret

--- Environments/Dev/main.tf
+++ Environments/Dev/main.tf
@@ -15,6 +15,9 @@ provider "azurerm" {
   features {}
   #subscription_id = "339e9158-9454-4c79-a362-c37d1f2469a2"  
   subscription_id = "c09e0f60-cb15-4c23-8500-eeae1ec9dd6b"
+ 
+  disable_correlation_request_id = true
+  disable_terraform_partner_id   = true
   #skip_provider_registration = true
   #  subscription_id = "c09e0f60-cb15-4c23-8500-eeae1ec9dd6b" # "az account show --query id -o tsv"
 }
