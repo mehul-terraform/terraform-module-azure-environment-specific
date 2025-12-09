782773df46768bf5b6aa87aeb1cfeba4ea97a4c9
Your Name <you@example.com>
2025-12-09 16:08:13 +0530

    addcode-working

--- Environments/Dev/main.tf
+++ Environments/Dev/main.tf
@@ -15,9 +15,9 @@ provider "azurerm" {
   features {}
   #subscription_id = "339e9158-9454-4c79-a362-c37d1f2469a2"  
   subscription_id = "c09e0f60-cb15-4c23-8500-eeae1ec9dd6b"
+  tenant_id = "8fc36c8e-1077-4442-a9a3-ef873f9cc6c7"
 
-  client_id       = "appId"
-  client_secret   = "password"
+
  
   disable_correlation_request_id = true
   disable_terraform_partner_id   = true
